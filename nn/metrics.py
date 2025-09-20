74bbd7750eef5675ca558aee0b5782b8140f57a4
Daniel Ige <dantomige@gmail.com>
2025-09-20 16:12:13 -0400

    metrics base classes

---
+++ nn/metrics.py
@@ -0,0 +1,27 @@
+
+
+class Metrics:
+    pass
+
+# categorical
+class Accuracy(Metrics):
+    pass
+
+class Precision(Metrics):
+    pass
+
+class Recall(Metrics):
+    pass
+
+class F1(Metrics):
+    pass
+
+class AUC_ROC(Metrics):
+    pass
+
+# regression
+class MSE(Metrics):
+    pass
+
+class R2(Metrics):
+    pass
