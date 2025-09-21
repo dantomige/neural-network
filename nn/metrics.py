4e0e946eb7fca52ae2770d5efa30af935b0b7c93
Daniel Ige <dantomige@gmail.com>
2025-09-21 00:42:22 -0400

    add metrics

--- nn/metrics.py
+++ nn/metrics.py
@@ -1,27 +1,101 @@
-
+from utils import dims, apply_func_matrix, matrix_add, scale_matrix
 
 class Metrics:
-    pass
+    
+    def __init__(self):
+        pass
+
+    def evaluate(self, Y, Yhat):
+        raise NotImplementedError
+
+    @staticmethod
+    def pred(Yhat):
+        logit_to_class = lambda p: 1 if p >= 0.5 else 0
+        Ypred = apply_func_matrix(logit_to_class, Yhat)
+        return Ypred
+
+    @staticmethod
+    def confusion_matrix(Y, Ypred):
+        assert dims(Y) == dims(Ypred)
+
+        TP = TN = FP = FN = 0
+        for rowY, rowYhat in zip(Y, Ypred):
+            is_true = rowY[0]
+            pred = rowYhat[0]
+
+            TP += is_true and pred
+            TN += is_true and not pred
+            FP += not is_true and pred
+            FN += not is_true and not pred
+        
+        return TP, TN, FP, FN
+                
 
 # categorical
 class Accuracy(Metrics):
-    pass
 
+    def evaluate(self, Y, Yhat):
+        Ypred = self.pred(Yhat)
+        TP, TN, FP, FN = self.confusion_matrix(Y, Ypred)
+        return (TP + TN)/(TP + TN + FP + FN)
+    
 class Precision(Metrics):
-    pass
+    
+    def evaluate(self, Y, Yhat):
+        Ypred = self.pred(Yhat)
+        TP, TN, FP, FN = self.confusion_matrix(Y, Ypred)
+        return TP/(TP + FP)
 
 class Recall(Metrics):
-    pass
+
+    def evaluate(self, Y, Yhat):
+        Ypred = self.pred(Yhat)
+        TP, TN, FP, FN = self.confusion_matrix(Y, Ypred)
+        return TP/(TP + FN)
 
 class F1(Metrics):
-    pass
+    def evaluate(self, Y, Yhat):
+        Ypred = self.pred(Yhat)
+        TP, TN, FP, FN = self.confusion_matrix(Y, Ypred)
+        precision = TP/(TP + FP)
+        recall = TP/(TP + FN)
+        return 2/(1/precision + 1/recall)
 
 class AUC_ROC(Metrics):
-    pass
+    def evaluate(self, Y, Yhat):
+        Ypred = self.pred(Yhat)
+        TP, TN, FP, FN = self.confusion_matrix(Y, Ypred)
+        return (TP + TN)/(TP + TN + FP + FN)
 
 # regression
 class MSE(Metrics):
-    pass
+    def evaluate(self, Y, Yhat):
+        assert dims(Yhat) == dims(Y)
 
-class R2(Metrics):
-    pass
+        M, N = dims(Yhat)
+        num_elts = M * N
+
+        error_vector = matrix_add(Yhat, scale_matrix(-1, Y))
+        square_error_vector = apply_func_matrix(lambda x : x ** 2, error_vector)
+        total_squared_error = sum(sum(row) for row in square_error_vector)
+
+        self.Yhat = Yhat
+        self.Y = Y
+
+        return total_squared_error/num_elts
+
+class MAE(Metrics):
+    def evaluate(self, Y, Yhat):
+        assert dims(Yhat) == dims(Y)
+
+        M, N = dims(Yhat)
+        num_elts = M * N
+
+        error_vector = matrix_add(Yhat, scale_matrix(-1, Y))
+        square_error_vector = apply_func_matrix(lambda x : abs(x), error_vector)
+        total_squared_error = sum(sum(row) for row in square_error_vector)
+
+        self.Yhat = Yhat
+        self.Y = Y
+
+        return total_squared_error/num_elts
